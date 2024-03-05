7e8120911801ba41d1a9f3daf7c0555809a35d0b
Begüm Ircı <begumirci@Begum-MacBook-Air.local>
2024-03-05 15:15:14 +0300

    değişiklik

--- src/components/category-section.jsx
+++ src/components/category-section.jsx
@@ -1,22 +1,36 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function CategorySection() {
-
-    return(
-        <div className='all-categories'>
-            <h1>Tüm Kategoriler</h1>
-            <div className='categories'>
-                <Link><img src='/public/makyaj.png' alt="" /></Link>
-                <Link><img src='/public/cilt.png' alt="" /></Link>
-                <Link><img src='/public/sac.png' alt="" /></Link>
-                <Link><img src='/public/parfum.png' alt="" /></Link>
-                <Link><img src='/public/erkekbakim.png' alt="" /></Link>
-                <Link><img src='/public/saglik.png' alt="" /></Link>
-                <Link><img src='/public/annebebek.png' alt="" /></Link>
-                <Link><img src='/public/evyasam.png' alt="" /></Link>
-            </div>
-        </div>
-    )
-    
+  return (
+    <div className='all-categories'>
+      <h1>Tüm Kategoriler</h1>
+      <div className='categories'>
+        <Link>
+          <img src='/public/makyaj.png' alt='' />
+        </Link>
+        <Link>
+          <img src='/public/cilt.png' alt='' />
+        </Link>
+        <Link>
+          <img src='/public/sac.png' alt='' />
+        </Link>
+        <Link>
+          <img src='/public/parfum.png' alt='' />
+        </Link>
+        <Link>
+          <img src='/public/erkekbakim.png' alt='' />
+        </Link>
+        <Link>
+          <img src='/public/saglik.png' alt='' />
+        </Link>
+        <Link>
+          <img src='/public/annebebek.png' alt='' />
+        </Link>
+        <Link>
+          <img src='/public/evyasam.png' alt='' />
+        </Link>
+      </div>
+    </div>
+  );
 }
